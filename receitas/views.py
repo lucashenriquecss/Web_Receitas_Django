350690e6cc5edec66ac47d641f7acf90991d8059
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2021-06-23 12:24:44 -0300

    att

--- receitas/views.py
+++ receitas/views.py
@@ -7,7 +7,8 @@ def index(request):
     receitas ={
         1:'Lasanha',
         2:'Sopa de legumes',
-        3:'Sorvete'
+        3:'Sorvete',
+        4:'Bolo de chocolate'
     }
     dados ={
         'nome_das_receitas' :receitas
