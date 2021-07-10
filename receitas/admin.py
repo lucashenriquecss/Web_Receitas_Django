aa5b9c2f5b877f67c35736f95b7a48b934658a61
lucashenriquecss <calheirossaraiva@hotmail.com>
2021-07-10 00:39:31 -0300

    att criação de banco pessoa

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from .models import Receitas
 
 # Register your models here.
+class ListandoReceitas(admin.ModelAdmin):#habilitando edição do admin 
+    list_display = ('id', 'nome_receita', 'categoria','tempo_preparo')  
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 5
 
-admin.site.register(Receitas)
+admin.site.register(Receitas, ListandoReceitas)
