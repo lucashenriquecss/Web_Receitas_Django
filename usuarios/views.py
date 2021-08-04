d25a50645116f421f2fa11ac2c6d93fc94877b61
lucashenriquecss <calheirossaraiva@hotmail.com>
2021-08-04 12:28:44 -0300

    att

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
-def cadastro(request):
-    return render(request,'usuarios/cadastro.html')
+def cadastro(request): # metodo post para cadastrar usuarios
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():# verificação para evitar campos em branco ou com espaços
+            print('O nome nao pode ser em branco!')
+            return redirect('cadastro')
+        if not email.strip():# verificação para evitar campos em branco ou com espaços
+            print('O email nao pode ser em branco!')
+            return redirect('cadastro')
+        if senha != senha2:# verificação para evitar campos em branco ou com espaços
+            print('O nome nao pode ser em branco!')
+            return redirect('cadastro')
+           
+        print(nome,email,senha,senha2)
+        return redirect('login')
+    else:
+        return render(request,'usuarios/cadastro.html')
 
 def login(request):
     return render(request,'usuarios/login.html')
