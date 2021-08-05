722821bb50df5b259827efcb0a03aea3e05ea4f3
lucashenriquecss <calheirossaraiva@hotmail.com>
2021-08-05 20:19:35 -0300

    att

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('logout', views.logout, name='logout'),
+    path('criar/receita', views.criar_receita, name='criar_receita'),
     
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def cadastro(request): # metodo post para cadastrar usuarios
@@ -7,6 +9,7 @@ def cadastro(request): # metodo post para cadastrar usuarios
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
+
         if not nome.strip():# verificação para evitar campos em branco ou com espaços
             print('O nome nao pode ser em branco!')
             return redirect('cadastro')
@@ -16,18 +19,47 @@ def cadastro(request): # metodo post para cadastrar usuarios
         if senha != senha2:# verificação para evitar campos em branco ou com espaços
             print('O nome nao pode ser em branco!')
             return redirect('cadastro')
-           
-        print(nome,email,senha,senha2)
+        if User.objects.filter(email=email).exists():
+            print('Usuario ja cadastrado!')
+            return redirect('cadastro') #verificando usuario se ja esta criado ou nao
+        user = User.objects.create_user(username=nome,email=email,password=senha)
+        user.save()
+        print('Usuario cadastradii ciom sucesso')
         return redirect('login')
     else:
         return render(request,'usuarios/cadastro.html')
 
+
 def login(request):
+    if request.method == 'POST': # mesmo metodo de verificação do cadastro
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email =="" or senha == "":# metedo para evitar que entre sem email e senha
+            print('campos nao podem ficar em branco')
+            return redirect('login')
+        print(email,senha)
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print("login realizado")      
+                return redirect('dashboard')
+
     return render(request,'usuarios/login.html')
 
+
 def logout(request):
-    pass
+    auth.logout(request)#saida do usuario
+    return redirect('index')
+    
 
 def dashboard(request):
-    pass
+    #verificar se usuario esta logado
+    if request.user.is_authenticated:      
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
 
+def criar_receita(request):
+    return render(request, 'usuarios/criar_receita.html')
